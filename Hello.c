84cc5fbf7d1ddbb86608f798f7a6c11e2db3f084
Manpreet Singh <Manpreetsingh966@yahoo.com>
2024-12-01 16:21:16 +0530

    Offline Tables Calculator

--- Hello.c
+++ Hello.c
@@ -1,12 +1,12 @@
-# include<stdio.h>
- int main() {
-
-   int a = 12 , b = 10 ;
-   printf("enter a");
-   printf("enter b");
-
-   printf("perimeter is : %d , 2 * a + b");
-
-
-
- }
+#include<stdio.h>
+int main(){
+    int a , b;
+    printf("Enter Number For Table: ");
+    scanf("%d" , &a);
+    printf("Enter Numbers Of Multiples: ");
+    scanf("%d" , &b);
+    
+    for(int i = 1 ; i<=b ; i++){
+        printf("%d \n" , a*i);
+    }
+}
